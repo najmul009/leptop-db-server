a4dfef3e4bb95aa3c8fe5dd8000b375d0cfb8fdb
najmul009 <najmul.tk16@gmail.com>
2022-05-10 14:25:33 +0600

    fetch data using ID

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
-
 const cors = require('cors')
-
 app.use(cors())
 app.use(express.json())
+require('dotenv').config();
 
 
 const uri = "mongodb+srv://leptopdb_009:nPGSOXPZGTawdKQM@cluster0.xhtnt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -28,6 +27,15 @@ async function run() {
             const items = await cursor.toArray()
             res.send(items)
         })
+
+        // Item Details
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const item = await leptopsCollection.findOne(query);
+            res.send(item);
+        });
         
     }
     finally{
